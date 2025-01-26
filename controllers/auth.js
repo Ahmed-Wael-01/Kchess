c1ea0e13dbf740d3a626608566e29348d74dd6dc
Ahmed-Wael-01 <Xahmedwaheed@gmail.com>
2025-01-26 20:21:41 +0200

    generate jwt tokens for auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,64 @@
+const con = require('../db');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const { name } = require('ejs');
+require('dotenv').config();
+
 exports.register = (req, res) => {
     console.log(req.body);
-    res.send('Form submitted')
+    const {fname, lname, email, username, dob, country, gender, password, rpassword} = req.body;
+    con.query('SELECT email FROM players WHERE email = ?', [email], async (err, result) => {
+        if(err) throw err;
+        if(result.length > 0) return res.render('signup', {
+            message: 'This email already exists'
+        });
+        if(password !== rpassword) return res.render('signup', {
+            message: 'passwords doesn\'t match'
+        });
+
+        let hashPass = await bcrypt.hash(password, 8);
+        
+        con.query('INSERT INTO players SET ?', {email: email,
+                                                password: hashPass,
+                                                country: country,
+                                                gender: gender,
+                                                fname: fname,
+                                                lname: lname,
+                                                birth_date: dob,
+                                                nickname: username
+        }, (err, results) => {
+            if(err) throw err;
+            else {
+                return res.render('signup', {
+                    message: 'Player registered successfuly'
+                });
+            }
+        });
+    });
+};
+
+exports.login = (req, res) => {
+    console.log(req.body);
+    const {usrOrEmail, password} = req.body;
+    con.query(`SELECT email, nickname, password FROM players WHERE email = '${usrOrEmail}' OR nickname = '${usrOrEmail}'`, async (err, result) => {
+        if(err) throw err;
+        if(result.length !== 1) return res.render('login', {
+            message: 'no user found'
+        });
+        console.log();
+        bcrypt.compare(password, result[0].password, (err, isRight) => {
+            if(err) throw err;
+            if(!isRight) {
+                return res.render('login', {
+                    message: 'password is incorrect'
+                })
+            } else {
+                const token = jwt.sign({usrOrEmail, password}, process.env.ACCESS_TOKEN_SECRET);
+                res.json({accessToken: token});
+                /*return res.render('login', {
+                    message: 'login success'
+                });*/
+            }
+        });
+    });
 };
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require('path');
 const con = require('./db');
 const jwt = require('jsonwebtoken');
+const { userInfo } = require('os');
+require('dotenv').config();
 
 const publicDirectory = path.join(__dirname, './public');
 app.use(express.static(publicDirectory));
@@ -13,8 +15,21 @@ app.use(express.json());
 
 app.set('view engine', 'ejs');
 
-app.use('/', require('./routes/pages'));
+app.use('/', authToken, require('./routes/pages'));
 app.use('/auth', require('./routes/auth'));
+
+function authToken(req, res, next){
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, userInfo) => {
+    if(err) return res.sendStatus(403);
+    req.user = userInfo.usrOrEmail;
+    next();
+  })
+}
 
 app.listen(3000, () => {
   console.log('server is running');
